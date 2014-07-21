6f864aee6f96f1911a20b9d67a5ac14c8d6a143d
dayAlone <andrey.slider@gmail.com>
2014-07-22 00:20:08 +0600

    Gallery

--- public_html/local/templates/kshve/components/bitrix/news.detail/news/template.php
+++ public_html/local/templates/kshve/components/bitrix/news.detail/news/template.php
@@ -8,5 +8,22 @@ $item = &$arResult;
 <div class="content no-bottom-padding small-top-padding small-bottom-margin">
 	<h1 class="center small-bottom-margin small-bottom-padding"><?=$item['NAME']?></h1>
 	<?=$item['~DETAIL_TEXT']?>
+	<?
+		if(count($item['IMAGES'])>0):?>
+		<div class="divider"></div>
+		<div class="gallery">
+		    <div class="slider">
+		        <? foreach ($item['IMAGES'] as $img):?>
+		            <div class="item">
+		                <a href="<?=$img['value']?>" rel="prettyPhoto[]" style="background-image: url(<?=$img['small']?>)" class="image">
+		                </a>
+		            </div>
+		        <? endforeach;?>
+		    </div>
+		</div>
+
+	<?
+		endif;
+	?>
 	<h1 class="center">Предыдущие новости</h1>
 </div>
