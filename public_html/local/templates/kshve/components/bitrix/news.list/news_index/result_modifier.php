3263e2b3e39b0c88ecfab548ad03892012b055ae
dayAlone <andrey.slider@gmail.com>
2014-07-26 14:19:42 +0600

    Changes

--- public_html/local/templates/kshve/components/bitrix/news.list/news_index/result_modifier.php
+++ public_html/local/templates/kshve/components/bitrix/news.list/news_index/result_modifier.php
@@ -1,7 +1,7 @@
 <?
 	foreach ($arResult['ITEMS'] as $key => &$item):
 		$item['DATE'] = strtotime($item['PROPERTIES']['DATE']['VALUE']);
-		$small = CFile::ResizeImageGet($item['PREVIEW_PICTURE'], Array("width" => 240, "height" => 180), BX_RESIZE_IMAGE_EXACT, false, false, false, 100);
+		$small = CFile::ResizeImageGet($item['PREVIEW_PICTURE'], Array("width" => 240, "height" => 180), BX_RESIZE_IMAGE_PROPORTIONAL, false, false, false, 100);
 		$item['PREVIEW_PICTURE']['SRC'] = $small['src'];
 	endforeach;
 ?>
--- public_html/local/templates/kshve/components/bitrix/news.list/partners/result_modifier.php
+++ public_html/local/templates/kshve/components/bitrix/news.list/partners/result_modifier.php
@@ -1,7 +1,7 @@
 <?
 	foreach ($arResult['ITEMS'] as $key => &$item):
 		$item['LINK'] = (strlen($item['PROPERTIES']['LINK']['VALUE'])>0?$item['PROPERTIES']['LINK']['VALUE']:"#");
-		$small = CFile::ResizeImageGet($item['PROPERTIES']['LOGO']['VALUE'], Array("width" => 90, "height" => 42), BX_RESIZE_IMAGE_EXACT, false, false, false, 100);
+		$small = CFile::ResizeImageGet($item['PROPERTIES']['LOGO']['VALUE'], Array("width" => 90, "height" => 42), BX_RESIZE_IMAGE_PROPORTIONAL, false, false, false, 100);
 		$item['LOGO'] = $small['src'];
 	endforeach;
 ?>
