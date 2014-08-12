da03f88370e413bd12a566a3b42e6cdaf40eb207
dayAlone <andrey.slider@gmail.com>
2014-08-12 19:02:36 +0600

    Changes

--- public_html/local/templates/kshve/components/bitrix/news.detail/list/result_modifier.php
+++ public_html/local/templates/kshve/components/bitrix/news.detail/list/result_modifier.php
@@ -21,8 +21,8 @@
     $description = $item['PROPERTIES'][$prop_name]['DESCRIPTION'];
 
     foreach ($item['PROPERTIES'][$prop_name]['VALUE'] as $key => $value):
-          $small = CFile::ResizeImageGet($value, Array("width" => 312, "height" => 312), BX_RESIZE_IMAGE_EXACT, false, false, false, 100);
-          $big = CFile::ResizeImageGet($value, Array("width" => 800, "height" => 700), BX_RESIZE_IMAGE_EXACT, false, false, false, 100);
+          $small = CFile::ResizeImageGet($value, Array("width" => 312, "height" => 312), BX_RESIZE_IMAGE_PROPORTIONAL, false, false, false, 100);
+          $big = CFile::ResizeImageGet($value, Array("width" => 800, "height" => 700), BX_RESIZE_IMAGE_PROPORTIONAL, false, false, false, 100);
           $gallery[] = array('sort'=>$description[$key], 'value'=> $big['src'], 'small'=> $small['src']);
     endforeach;
 
